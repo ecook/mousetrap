8cda6a4c8171d7067324a7a82401f5da95bcae3a
Ed Cook <ed.cook@icg360.com>
2019-03-15 16:19:58 -0700

    fixed avatar image

--- src/loggedin.js
+++ src/loggedin.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavDropdown } from "react-bootstrap";
+import { NavDropdown, Image } from "react-bootstrap";
 
 export default (props) => {
 
@@ -12,8 +12,8 @@ export default (props) => {
     return (
         <React.Fragment>
             <span>hello {props.profile.login}</span> 
-            <img className={`avatar ${props.profile.avatar}`} src="./images/img_trans.jpg" width="1" height="1" />
-            <NavDropdown title="Dropdown" id="nav-dropdown"  onSelect={k => handleSelect(k)}>
+            <Image className={`avatar ${props.profile.avatar}`}  roundedCircle />
+            <NavDropdown title="" id="nav-dropdown"  onSelect={k => handleSelect(k)}>
                 {props.profile.isAdmin && <NavDropdown.Item eventKey="login">user management</NavDropdown.Item> }
                 <NavDropdown.Item eventKey="profile">my Profile</NavDropdown.Item>
                 <NavDropdown.Item eventKey="logout">Logout</NavDropdown.Item>
